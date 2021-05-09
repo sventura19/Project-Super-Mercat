cf17649772e8c3328a201878ec85bbc70464d321
sventura19 <sventura19@jaumebalmes.net>
2021-05-09 20:21:05 +0200

    update

--- SuperMercat/src/SuperMercat.java
+++ SuperMercat/src/SuperMercat.java
@@ -1,23 +1,17 @@
-import java.io.BufferedReader;
+﻿import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 
 public class SuperMercat {
-    public static void main(String[] args) throws IOException {
+    public static void main (String [] args) throws IOException {
 
-        Datos("D:\\sergi\\Documents\\IdeaProjects\\XML\\Supermercat\\src\\Stock.txt");
+        Datos("Stock.txt");
         segon.saluda();
         //leerStock();
     }
 
-    void menuPrincipal(){
-        System.out.println("Gestio de supermercat");
-        System.out.println("1. Gestio Stock");
-        System.out.println("2.");
-    }
 /*
     static void leerStock(){
         Scanner e = new Scanner(System.in);
