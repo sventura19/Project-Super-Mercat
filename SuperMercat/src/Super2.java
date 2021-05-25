5c10798e8b0218f5e2e26d8eb027e8f430baabf4
sventura19 <sventura19@jaumebalmes.net>
2021-05-25 15:56:03 +0200

    update

--- SuperMercat/src/Super2.java
+++ SuperMercat/src/Super2.java
@@ -1,68 +1,91 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.util.Scanner;
 import java.io.*;
 
 public class Super2 {
+    /* he decidido poner el menú principal como una función ya que, creo que es más cómodo poder manipular el código
+    desde una función y mantener el main solamente con ello.
+     */
     public static void main(String[] args) {
         menuPrincipal();
-        String [][] stock = new String [1000][3];
-        while (stock != null);
-        rellenaStock(stock);
     }
     public static void menuPrincipal() {
         Scanner entrada = new Scanner(System.in);
         int opció = 0;
+        System.out.println("******************* SUPERMERCAT *********************");
+        System.out.println(" ");
+        System.out.println("------------------ Menú principal -------------------");
+        System.out.println(" ");
+        System.out.println("----------------- Escull una opció ------------------");
+        System.out.println(" ");
+        System.out.println("                    1. Stock                         ");
+        System.out.println("                    2. Vendes                        ");
+        System.out.println("                    3. Ajuda                         ");
+        System.out.println("                    4. Sortida                       ");
+        System.out.println(" ");
+        System.out.println("-----------------------------------------------------");
+        opció = entrada.nextInt();
+            /*
+            dentro del menú he añadido una opción extra de ayuda, en caso de que el usuario no tenga claro el qué hace cada opción.
+            * todo hecho con condicionales para comodidad.
+             */
+        if (opció == 1) {
+            mostraMenuStock();
+        } else if (opció == 2) {
+            mostraMenuVendes();
+        } else if (opció == 3) {
+            mostraAjuda();
+        } else if (opció == 4) {
+            System.exit(0);
+        }
+    }
+    /*
+    en la función mostraMenuStock he montado la primera parte ya preparada para registrar y almacenar los productos
+    se crea la matriz para almacenar y mostrar los productos. Ésta misma parte se utilizará para el código de ventas y la
+    generación del ticket.
+     */
+    public static void mostraMenuStock() {
+        Scanner entrada = new Scanner(System.in);
+        String[][] stock = new String[1000][3];
+        String palabra = "";
+        int x = 0;
+        int y = 0;
+        try (Scanner entradaStock = new Scanner(new File("D:\\sergi\\Documents\\IdeaProjects\\Project-Super-Mercat\\SuperMercat\\src\\Stock.txt"))) {
+            while (entradaStock.hasNext()) {
+                palabra = entradaStock.next();
+                stock[x][y] = palabra;
+                y++;
+                if (y == 3) {
+                    x++;
+                    y = 0;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        int opció = 0;
         do {
             System.out.println("******************* SUPERMERCAT *********************");
             System.out.println(" ");
-            System.out.println("------------------ Menú principal -------------------");
+            System.out.println("---------------------  Stock  -----------------------");
             System.out.println(" ");
             System.out.println("----------------- Escull una opció ------------------");
             System.out.println(" ");
-            System.out.println("                    1. Stock                         ");
-            System.out.println("                    2. Vendes                        ");
-            System.out.println("                    3. Ajuda                         ");
-            System.out.println("                    4. Sortida                       ");
+            System.out.println("                    1. Afegir                        ");
+            System.out.println("                    2. Consultar                     ");
+            System.out.println("                    3. Sortir                        ");
             System.out.println(" ");
             System.out.println("-----------------------------------------------------");
             opció = entrada.nextInt();
             if (opció == 1) {
-                mostraMenuStock();
+                menuAfegir();
             } else if (opció == 2) {
-                mostraMenuVendes();
+                mostraArrayStock(stock, x);
             } else if (opció == 3) {
-                mostraAjuda();
-            } else if (opció == 4) {
-                System.exit(0);
+                menuPrincipal();
             }
         }
-        while (opció > 4 || opció < 0);
-        System.out.println("no és una opció correcta, escull entre");
-    }
-    public static void mostraMenuStock() {
-        Scanner entrada = new Scanner(System.in);
-        int opció = 0;
-        System.out.println("******************* SUPERMERCAT *********************");
-        System.out.println(" ");
-        System.out.println("---------------------  Stock  -----------------------");
-        System.out.println(" ");
-        System.out.println("----------------- Escull una opció ------------------");
-        System.out.println(" ");
-        System.out.println("                    1. Afegir                        ");
-        System.out.println("                    2. Consultar                     ");
-        System.out.println("                    3. Sortir                        ");
-        System.out.println(" ");
-        System.out.println("-----------------------------------------------------");
-        opció = entrada.nextInt();
-        if (opció == 1) {
-            menuAfegir();
-        } else if (opció == 2) {
-            mostraStock();
-        } else if (opció == 3) {
-            menuPrincipal();
-        }
+        while (opció != 3);
     }
     public static void menuAfegir(){
         Scanner entrada = new Scanner (System.in);
@@ -71,18 +94,39 @@ public class Super2 {
         System.out.println(" ");
         System.out.println("----------------  Afegir productes  -----------------");
         System.out.println(" ");
-        System.out.println("    Codi          Nom del producte         Preu      ");
-        System.out.println("   ------       -------------------       ------     ");
+        System.out.println("Codi - Nom del producte - Preu");
+        producte = entrada.nextLine();
         try{
             FileWriter fw = new FileWriter("D:\\sergi\\Documents\\IdeaProjects\\Project-Super-Mercat\\SuperMercat\\src\\Stock.txt",true);
             fw.write(producte);
+            fw.write("\r\n");
             fw.close();
         }
         catch(IOException ex){
             System.out.println("fitxer no trobat");
         }
-
     }
+    public static void mostraArrayStock(String[][]stock, int x){
+        System.out.println("******************* SUPERMERCAT *********************");
+        System.out.println(" ");
+        System.out.println("----------------  Afegir productes  -----------------");
+        System.out.println(" ");
+        System.out.println("Codi           Nom del producte         Preu      ");
+        System.out.println("-----         ------------------       ------      ");
+        for (int n =0; n < x; n++){
+            for (int j =0; j < 3; j++){
+                System.out.printf("%-20s",stock[n][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println(" ");
+        System.out.println("------------------------------------------------------");
+        System.out.println(" ");
+    }
+    /*
+    el código lo hemos pasado fuera del código principal porque ponerlo aquí saturaría y sería más lioso de entender.
+    lo llamo a través de otro archivo.
+     */
     public static void mostraMenuVendes(){
         Scanner entrada = new Scanner (System.in);
         int opció=0;
@@ -98,7 +142,7 @@ public class Super2 {
         System.out.println("-----------------------------------------------------");
         opció=entrada.nextInt();
         if(opció == 1){
-            //   començaTicket();
+            ventasEdu.mostraMenuCompra();
         }
         else if (opció == 2){
             menuPrincipal();
@@ -123,73 +167,5 @@ public class Super2 {
             menuPrincipal();
         }
     }
-    public static void rellenaStock(String[][]stock){
-        String palabra="";
-        int x=0;
-        int y=0;
-        try(Scanner entradaStock = new Scanner (new File("D:\\sergi\\Documents\\IdeaProjects\\Project-Super-Mercat\\SuperMercat\\src\\Stock.txt"))){
-            while(entradaStock.hasNext()){
-                palabra=entradaStock.next();
-                stock[x][y]=palabra;
-                y++;
-                if(y==3){
-                    x++;
-                    y=0;
-                }
-            }
-        }
-        catch(FileNotFoundException e){
-            System.out.println(e);
-
-        }
-        String producte="";
-        for (int n = 0; n < stock.length; n++){
-            for (int j = 0; j < stock[n].length; j++){
-                // stock[n][j]=;
-            }
-        }
-    }
-    public static void mostraStock(){
-        int volver;
-        Scanner entrada = new Scanner(System.in);
-        File stock = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-        try {
-            stock = new File("Stock.txt");
-            fr = new FileReader("D:\\sergi\\Documents\\IdeaProjects\\Project-Super-Mercat\\SuperMercat\\src\\Stock.txt");
-            br = new BufferedReader(fr);
-            String mostraStock;
-            while ((mostraStock = br.readLine()) != null)
-                System.out.println(mostraStock);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        }
-        System.out.println("per tornar, prem 4");
-        volver=entrada.nextInt();
-        mostraMenuStock();
-    }
-
-    /*public static void mostraAfegir(){
-        String producte="";
-        try{
-            FileWriter fw = new FileWriter("/Users/eduardoroldan/IdeaProjects/Project-Super-Mercat/SuperMercat/src/Stock.txt",true);
-            fw.write(producte);
-            fw.close();
-        }
-        catch(IOException ex){
-            System.out.println("fitxer no trobat");
-        }
-
-    }
-*/
 }
 
